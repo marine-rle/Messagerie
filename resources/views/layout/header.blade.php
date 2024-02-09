8134e560c25133ac81e55bf7aa073980b2bc991f
marine-rle <120376794+marine-rle@users.noreply.github.com>
2024-02-09 15:05:38 +0100

    update header.blade.php style

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -5,12 +5,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{('Messagerie')}}</title>
+    <title>{{__('Messagerie')}}</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <style>
         .navbar-dark-custom {
             background-color: #343a40 !important; /* Couleur de fond foncée */
             color: #ffffff !important; /* Couleur de texte blanche */
+        }
+
+        /* Mettre en majuscule la première lettre du nom */
+        .capitalize {
+            text-transform: capitalize;
         }
     </style>
 </head>
@@ -18,11 +23,11 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark navbar-dark-custom">
         <div class="container-fluid">
-            <div class="navbar-brand"> 💬 {{('Messagerie')}}</div>
+            <div class="navbar-brand"> 💬 {{__('Messagerie')}}</div>
             <div class="d-flex justify-content-center align-items-center flex-grow-1">
                 <div class="text-center">
-                    <div class="font-medium text-base text-white">
-                        {{('Bonjour')}} {{ Auth::user()->name }} !
+                    <div class="font-medium text-base text-white capitalize">
+                        {{__('Bonjour')}} {{ ucfirst(Auth::user()->name) }} ! <!-- Utilisation de ucfirst() pour mettre en majuscule la première lettre -->
                     </div>
                 </div>
             </div>
